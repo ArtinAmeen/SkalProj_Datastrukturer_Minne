78c987d59f95045e9e143c372d4ccb05c7fd14ba
Artin <alex_91_100@live.se>
2021-12-20 16:32:51 +0100

    Add project files.

---
+++ SkalProj_Datastrukturer_Minne/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SkalProj_Datastrukturer_Minne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+            List<string> stringList = new List<string>();
+            string input = Console.ReadLine();
+
+            char[] str = new char[input.Length - 1];
+
+            if (input[0] == '+')
+            {
+                for (int i = 1; i < input.Length; i++)
+                {
+                    str[i - 1] = input[i];
+                }
+                // string newElement = string.Join(str);
+            }
+
+            else if (input[0] == '-')
+            {
+            }
+        }
+
+    }
+
+        public void ExamineList()
+        {
+        }
+    }
+}
